f9ad55e76dea308aebe5ff1672d0206fb81379c2
Gonzalo Núñez <49925953+gnunezm@users.noreply.github.com>
2022-03-22 14:07:43 -0300

    Update ecmo.R

--- ecmo.R
+++ ecmo.R
@@ -8,11 +8,14 @@ ipak <- function(pkg){
 }
 ipak(librerias)
 
+#Definiendo ruta de acceso
 getwd()
 setwd("/Volumes/GoogleDrive/Otros ordenadores/Desktop/Proyectos 2022/Proyecto ECMO/")
 
+#Cargando base de datos
 ecmo <-read_excel('ecmo.xlsx',col_names=TRUE) #cargando BBDD
 
+#Filtrando la información requerida
 ecmo1<-ecmo %>%
     filter(!is.na(INSTALACIÓN) & !is.na(`FECHA RETIRO`)) %>%  #removiendo NA
     arrange(-ID) %>%
@@ -23,9 +26,10 @@ ecmo1<-ecmo %>%
     group_by(date) %>%
     summarise(conteo=n()) #conteo de casos por fecha
 
+#Generando mapa
 ggplot_calendar_heatmap(
-    ecmo1,'date','conteo') +
-    ggtitle('Conteo Casos de Asistencia Extracorpórea durante años 2019 a 2021') +
-    xlab('Mes') + ylab('Días de la Semana') +
-    scale_fill_continuous(name='Nº Casos',low='#f9f90a',high='#e03812',breaks=seq(1,10,1)) +
-    facet_wrap(~Year,ncol=1)
+    ecmo1,'date','conteo') + #Definiendo base a utilizar, eje 'X' y eje 'Y'
+    ggtitle('Conteo Casos de Asistencia Extracorpórea durante años 2019 a 2021') + #Colocando título
+    xlab('Mes') + ylab('Días de la Semana') + # Nombres de ejes
+    scale_fill_continuous(name='Nº Casos',low='#f9f90a',high='#e03812',breaks=seq(1,10,1)) + # Generando leyenda
+    facet_wrap(~Year,ncol=1) # Permite mostrar cada año como una columna
